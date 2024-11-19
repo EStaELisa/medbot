e75f485fea08c9e18a28f45acd6424409cf9bd7c
eddaaa <edda.gieseler@gmail.com>
2024-11-19 09:08:17 +0100

    #3: add scrolling

--- src/components/body/Body.tsx
+++ src/components/body/Body.tsx
@@ -1,19 +1,31 @@
 import React from 'react'
 import styles from './body.module.css'
 import ChatMessages from '../chatMessages/ChatMessages'
+import Input from '../input/Input'
 
 const Body = () => {
   const messages = [
     { sender: 'Me', content: 'Hello', timeStamp: Date.now(), isOutgoing: true},
     { sender: 'MedChat', content: 'Hello! How can I help you?', timeStamp: Date.now(), isOutgoing: false},
     { sender: 'Me', content: 'I just want to chat!', timeStamp: Date.now(), isOutgoing: true},
+    { sender: 'Me', content: 'Hello', timeStamp: Date.now(), isOutgoing: true},
+    { sender: 'MedChat', content: 'Hello! How can I help you?', timeStamp: Date.now(), isOutgoing: false},
+    { sender: 'Me', content: 'I just want to chat!', timeStamp: Date.now(), isOutgoing: true},
+    { sender: 'Me', content: 'Hello', timeStamp: Date.now(), isOutgoing: true},
+    { sender: 'MedChat', content: 'Hello! How can I help you?', timeStamp: Date.now(), isOutgoing: false},
+    { sender: 'Me', content: 'I just want to chat!', timeStamp: Date.now(), isOutgoing: true},
   ]
 
   return (
-    <div>
-      {messages.map((msg, idx) => (
-        <ChatMessages key={idx} sender={msg.sender} content={msg.content} timestamp={msg.timeStamp} isOutgoing={msg.isOutgoing}/>
-      ))}
+    <div className={styles['chatContainer']}>
+      <div className={styles['messageContainer']}>
+        {messages.map((msg, idx) => (
+          <ChatMessages key={idx} sender={msg.sender} content={msg.content} timestamp={msg.timeStamp} isOutgoing={msg.isOutgoing}/>
+        ))}
+      </div>
+      <div className={styles['inputContainer']}>
+        <Input/>
+      </div>
     </div>
   )
 }
