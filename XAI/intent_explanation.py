c976966f7a216525a6122749ed87af906c848352
talviphoenix <talviphoenix@gmail.com>
2025-01-07 18:46:22 +0100

    feat: intent explanation with lime

---
+++ XAI/intent_explanation.py
@@ -0,0 +1,37 @@
+import webbrowser
+
+from lime.lime_text import LimeTextExplainer
+import os
+import torch
+
+class ModelWrapper:
+    def __init__(self, model, tokenizer, label_map):
+        self.model = model
+        self.tokenizer = tokenizer
+        self.label_map = label_map
+        self.reverse_label_map = {v: k for k, v in label_map.items()}
+
+    def predict_proba(self, texts):
+        """
+        Takes a list of texts and return probabilities for each label
+        """
+        inputs = self.tokenizer(texts, padding=True, truncation=True, return_tensors="pt")
+        outputs = self.model(**inputs)
+        logits = outputs.logits.detach().numpy()
+        probs = torch.nn.functional.softmax(torch.tensor(logits), dim=-1).numpy()
+        return probs
+
+def lime_explanation(model_wrapper, text):
+    explainer = LimeTextExplainer(class_names=list(model_wrapper.label_map.keys()))
+
+    explanation = explainer.explain_instance(
+        text,
+        model_wrapper.predict_proba,
+        num_features=5,
+        num_samples=100
+    )
+
+    explanation_path = "../XAI/tmp/lime_explanation.html"
+    explanation.save_to_file(explanation_path)
+    # TODO change default html application
+    webbrowser.get("firefox").open(f"file://{os.path.abspath(explanation_path)}")
--- main/main.py
+++ main/main.py
@@ -1,12 +1,24 @@
+import os
+import webbrowser
+
 from AnonymizationPipeline import anonymize
-from XAI import anonymization_explanation
+from XAI import anonymization_explanation, intent_explanation
+from tests.test_questiontosql import test_intent
+from transformers import BertForSequenceClassification, BertTokenizer
+
 
 def main():
-    text = input()
+    MODEL_PATH = "DeliaMo/ner_intent"
+    model = BertForSequenceClassification.from_pretrained(MODEL_PATH)
+    tokenizer = BertTokenizer.from_pretrained(MODEL_PATH)
+
+    text = "The patient Julia Meyer is suffering from fewer and chills, what could her diagnose be? Call her back at +491110020"
     anon_text, entities = anonymize.anonymize_prompt(text)
     print(anon_text)
     anonymization_explanation.explain_anonymization(entities)
 
+    wrapper = intent_explanation.ModelWrapper(model, tokenizer, {"get_symptoms": 0, "get_diagnose": 1})
+    intent_explanation.lime_explanation(wrapper, anon_text)
 
 if __name__ == "__main__":
     main()
