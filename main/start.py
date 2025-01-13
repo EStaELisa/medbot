c34b7f2c5f8c34554af9e71332ef22b3e47ce3fc
Esta Kempf <63785314+EStaELisa@users.noreply.github.com>
2025-01-13 16:00:38 +0100

    Add start function to process queries and generate SQL responses (#4)

---
+++ main/start.py
@@ -0,0 +1,22 @@
+from questiontosql.to_sql import predict_intent, extract_entities, generate_sql
+
+
+def start(query):
+
+    # ANONYMIZATION HERE
+    query_anon = query
+
+    # build sql query
+    intent = predict_intent(query_anon)
+    entities = extract_entities(query_anon)
+    sql_query = generate_sql(intent, entities)
+
+    # get result from database
+
+    # generate text response
+    if intent == "get_symptoms":
+        response = f"The symptoms for {', '.join(entities)} are: "
+    elif intent == "get_diagnose":
+        response = f"Possible diagnoses for the symptoms {', '.join(entities)} are: "
+
+    return response
